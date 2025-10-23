0f3b7f7638c650e35f77ba8ca2582c52384c978a
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-10-23 15:08:44 -0300

    Trabalhando com variáveis e constantes

---
+++ variaveis_e_constantes.py
@@ -0,0 +1,24 @@
+# Criando as variáveis age e name e apresentando na tela
+age = 37
+name = "Marcos"
+print(f"Meu nome é {name} e eu tenho {age} ano(s) de idade.")
+# Modificando as variáveis age e name e apresentando novamente na tela
+age = 25
+name = "Ana"
+print(f"Meu nome é {name} e eu tenho {age} ano(s) de idade.")
+
+limite_saque_diario = 1000
+print(limite_saque_diario)
+
+# Exemplos de Constantes no Python
+DOCS_PATH = '/home/marcos/Documentos'
+DEBUG = True
+AMOUNT = 30.2
+STATES = [
+    'SP',
+    'RJ',
+    'MG'
+]
+
+print(STATES)
+# A constante é definida tendo todas as letras maiúsculas no nome
