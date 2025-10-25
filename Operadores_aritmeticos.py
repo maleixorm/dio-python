65b69540b44eb6714608b59840d496d1e751e3c7
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-10-25 18:34:08 -0300

    Operadores aritméticos no Python

---
+++ Operadores_aritmeticos.py
@@ -0,0 +1,9 @@
+print(1 + 1) # Adição
+print(10 - 2) # Subtração
+print(3 * 4) # Multiplicação
+print(12 / 3) # Divisão
+print(12 // 2) # Divisão por inteiro
+print(10 % 3) # Resto da Divisão
+print(2 ** 3) # Exponenciação
+
+print(10-5*2)
