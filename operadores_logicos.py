ca168a16e88a02309b4b78b20f8f121ab3a10eef
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-10-25 19:06:06 -0300

    update operadores logicos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -3,6 +3,8 @@ saque = 250
 limite = 200
 cheque_especial = True
 
-saldo >= saque and saque <= limite or cheque_especial and saldo >= saque
+exp = saldo >= saque and saque <= limite or cheque_especial and saldo >= saque
+print(exp)
 
-(saldo >= saque and saque <= limite) or (cheque_especial and saldo >= saque)
+exp_2 = (saldo >= saque and saque <= limite) or (cheque_especial and saldo >= saque)
+print(exp_2)
