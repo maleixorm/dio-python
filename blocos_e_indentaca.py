95b9a46c2764e6b3f7d21810186c591577cdd1f2
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-10-26 21:15:34 -0300

    Criando a função depositar e utilizando a variável saldo nas funções

--- blocos_e_indentaca.py
+++ blocos_e_indentaca.py
@@ -1,11 +1,24 @@
+saldo = 500
+
 def sacar(valor):
-    saldo = 500
+    global saldo
     if saldo >= valor:
         saldo -= valor
         print("Saque realizado!")
-        print(f"O valor remanescente na sua conta é R$ {saldo}")
+        print(f"Saldo: R$ {saldo}")
     else:
         print("Saque Recusado!")
         print("O valor socilitado é maior que o seu saldo.")
 
-sacar(1500)
+def depositar(valor):
+    global saldo
+    if valor > 0:
+        saldo += valor
+        print("Saldo atualizado após sucesso no depósito")
+        print(f"Saldo: R$ {saldo}")
+    else:
+        print("Depósito Recusado!")
+        print("O valor depositado em conta deve ser maior que zero.")
+
+sacar(150)
+depositar(300)
