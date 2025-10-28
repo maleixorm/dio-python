02c70233b5974c1df3b2cdae0a7eed2f43e003c2
Marcos Aleixo <maknytz@gmail.com>
2025-10-28 16:56:24 -0300

    Trabalhando com strings de múltiplas linhas

---
+++ string_4.py
@@ -0,0 +1,23 @@
+nome = "Marcos"
+
+mensagem = f"""
+    Olá, meu nome é {nome}.
+Eu estou aprendendo Python
+        Esta mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+print("""
+    ########## MENU ##########
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+      
+    ##########################
+    
+      
+      Obrigado por utilizar nosso sistema!
+
+""")
