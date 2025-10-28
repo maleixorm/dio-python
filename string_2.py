bc319a24121452eb0b634ce52b76df03f254a468
Marcos Aleixo <maknytz@gmail.com>
2025-10-28 14:00:43 -0300

    Interpolando strings no Python

--- string_2.py
+++ string_2.py
@@ -2,6 +2,7 @@ nome = "Marcos"
 idade = 37
 profissao = "Programador"
 linguagem = "Python"
+saldo = 45.4345
 
 dados = {"nome": "Marcos", "idade": 37}
 
@@ -10,4 +11,4 @@ print("Nome: {} Idade: {}".format(nome, idade))
 print("Nome: {0} Idade: {1}".format(nome, idade))
 print("Nome: {name} Idade: {age}".format(age=idade, name=nome))
 print("Nome: {nome} Idade: {idade}".format(**dados))
-print(f"Nome: {nome} Idade: {idade}")
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:.2f}")
