6f2b3682afb919ad4ee0a85e3ba8ffb851309521
Marcos Aleixo <maknytz@gmail.com>
2025-10-28 11:44:52 -0300

    Funções para junção e concatenação de strings

--- string_1.py
+++ string_1.py
@@ -10,3 +10,14 @@ print(texto)
 print(texto.strip() + ".")
 print(texto.rstrip() + ".")
 print(texto.lstrip() + ".")
+
+menu = "Python"
+
+print("####" + menu + "####")
+print(menu.center(14))
+print(menu.center(20, "#"))
+print("-".join(menu))
+
+for letra in menu:
+    print(letra, end="-")
+print()
