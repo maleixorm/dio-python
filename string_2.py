78d4e3de7d93bb4bc31ad03b75c150b90ed6933b
Marcos Aleixo <maknytz@gmail.com>
2025-10-28 13:55:38 -0300

    Interpolação de variáveis

---
+++ string_2.py
@@ -0,0 +1,13 @@
+nome = "Marcos"
+idade = 37
+profissao = "Programador"
+linguagem = "Python"
+
+dados = {"nome": "Marcos", "idade": 37}
+
+print("Nome: %s Idade: %d" % (nome, idade))
+print("Nome: {} Idade: {}".format(nome, idade))
+print("Nome: {0} Idade: {1}".format(nome, idade))
+print("Nome: {name} Idade: {age}".format(age=idade, name=nome))
+print("Nome: {nome} Idade: {idade}".format(**dados))
+print(f"Nome: {nome} Idade: {idade}")
