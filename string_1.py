f18a75f12c4a19133b93e3437cb1256a935b4c71
Marcos Aleixo <maknytz@gmail.com>
2025-10-28 11:40:02 -0300

    Funções de string para espaços em branco

--- string_1.py
+++ string_1.py
@@ -4,3 +4,9 @@ print(nome.upper())
 print(nome.lower())
 print(nome.title())
 
+texto = "   Olá mundo    "
+
+print(texto)
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
